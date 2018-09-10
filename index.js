0b67b309f3d333ff85eb885c3132153656a0dc41
Bekzod <bek.abakulov@gmail.com>
2018-09-10 11:57:24 +0500

    Parrot Skill

--- index.js
+++ index.js
@@ -1,13 +1,74 @@
-var http = require('http');
+const express = require('express');
 
-var server = http.createServer(function(request, response) {
+const port = process.env.PORT || 3000;
+const app = express();
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+app.use(express.json());
 
+app.post('/', function (req, res) {
+
+  if (req.body.request.command == "no text")
+  {
+    res.json({
+      version: req.body.version,
+      session: req.body.session,
+      response: {
+        text: "",
+        end_session: false,
+      },
+    });
+  }
+
+  else if (req.body.request.command == "no version")
+  {
+    res.json({
+      session: req.body.session,
+      response: {
+        text: req.body.request.command || 'Hello!',
+        end_session: false,
+      },
+    });
+  }
+
+  else if (req.body.request.command == "no session")
+  {
+    res.json({
+      version: req.body.version,
+      response: {
+        text: req.body.request.command || 'Hello!',
+        end_session: false,
+      },
+    });
+  }
+
+  else if (req.body.request.command == "end session")
+  {
+    res.json({
+      version: req.body.version,
+      session: req.body.session,
+      response: {
+        text: req.body.request.command || 'Hello!',
+        end_session: true,
+      },
+    });
+  }
+
+  else 
+  {
+    res.json({
+      version: req.body.version,
+      session: req.body.session,
+        response: {
+          text: req.body.request.command || 'Hello!',
+          
+          end_session: false,
+        },
+    })
+  ;}
 });
 
-var port = process.env.PORT || 1337;
-server.listen(port);
+app.use('*', function (req, res) {
+  res.sendStatus(404);
+});
 
-console.log("Server running at http://localhost:%d", port);
+app.listen(port);
